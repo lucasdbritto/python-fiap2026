a2b7566c3fc4b104973f5f918b3940f31d71e23d
lucasdbritto <lucasdbritto26@gmail.com>
2026-03-17 11:44:08 -0300

    aula 2

---
+++ aula-02-variaveis-operadores/Desafio-aula02a.py
@@ -0,0 +1,11 @@
+Nome = input("digite seu nome:")
+print(f"boas-vindas, {Nome}")
+
+ano_nascimento = int(input("digite o ano que você nasceu:"))
+mes_nascimento = int(input("digite o mês que você nasceu:"))
+dia_nascimento = int(input("digite o dia em que você nasceu:"))
+print(f"{Nome} você nasceu em: {dia_nascimento}/{mes_nascimento}/{ano_nascimento}")
+
+num1 = int(input("digite um número:"))
+num2 = int(input("digite outro número:"))
+print(f"a soma de {num1} e {num2} é:", num1+num2)
---
+++ aula-02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,20 @@
+print("Olá mundo!")
+print(7+4)
+print("7"+"4")#concatenação de strings
+print ("7+4")
+'''
+comentarios de multiplas linhas
+'''
+# Variaveis
+nome="Lucas"
+idade=19
+peso=73.0
+
+print(nome,idade,peso)
+print(f"oi,{nome}!")
+
+#input simula um formulário no cmd
+nome = input("digite seu nome:")
+idade = int(input("digite sua idade:"))
+peso = float(input("digite seu peso:"))
+print(nome, idade, peso)
